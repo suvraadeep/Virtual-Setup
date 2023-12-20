47a72b08bbcca8b77d963316b4a2173e07bfdf4d
suvraadeep <dassuvradeep9@gmail.com>
2023-12-21 01:44:21 +0530

    edited code

--- src/main/handgesture.py
+++ src/main/handgesture.py
@@ -11,34 +11,25 @@ mpDraw = mp.solutions.drawing_utils
 handsconnection = mpHands.HAND_CONNECTIONS
 
 while True:
-    success, img = cap.read()
-    cv2.imshow("image", img)
+    rv, img = cap.read()
 
-    imgRGB = cv2.cvtColor(cv2.flip(img, 1), cv2.COLOR_BGR2RGB)
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     results = hands.process(imgRGB)
-    if cv2.waitKey(1)==13:
-        break
-
-
     if results.multi_hand_landmarks:
-
         for handLms in results.multi_hand_landmarks:
             for id, lm in enumerate(handLms.landmark):
-                h, w, c = img.shape
-                nx, ny, nz = int(lm.x * w), int(lm.y * h), int(lm.z * c)
-                print(id, ":", nx, ny, nz)
-            # mpDraw.draw_landmarks(img, handLms, handsconnection)
+                h,w,c = img.shape
+                cx,cy,cz = int(lm.x * w),int(lm.y * h),int(lm.y * c)
+                print(id,":",cx,cy,cz)
+            #mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
 
+    cTime=time.time()
+    fps =1/(cTime - pTime)
+    pTime=cTime
+    cv2.putText(img, str(int(fps)), (20, 80), cv2.FONT_HERSHEY_COMPLEX_SMALL, 3,(255, 0, 255), 3)
 
-        def coordinate(results, landmark, num):
-
-            return float(str(results.multi_hand_landmarks[-1].landmark[landmark]).split('\n')[num].split(" ")[1])
-    cTime = time.time()
-    fps = 1 / (cTime - pTime)
-    pTime = cTime
+    cv2.imshow("Camera", img)
     
+    if cv2.waitKey(1) == 13:
+        break
 cv2.destroyWindow()
-
-
-
-
