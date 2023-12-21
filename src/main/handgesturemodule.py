3b12b2abcb11d2a849df7d228bc02465aa09de53
suvraadeep <dassuvradeep9@gmail.com>
2023-12-21 20:19:45 +0530

    module created

--- src/main/handgesturemodule.py
+++ src/main/handgesturemodule.py
@@ -2,4 +2,68 @@ import cv2
 import mediapipe as mp
 import time
 
+
+class handDetector():
+    
+    def __init__(self, mode=False, maxHands=2,modelComp=1, detectionCon=0.5, trackCon=0.5):
+        self.mode = mode
+        self.maxHands = maxHands
+        self.modelComp=modelComp
+        self.detectionCon = detectionCon
+        self.trackCon = trackCon
+        self.mpHands = mp.solutions.hands
+        self.hands = self.mpHands.Hands(self.mode, self.maxHands,self.modelComp,self.detectionCon, self.trackCon)
+        self.mpDraw = mp.solutions.drawing_utils
         
+        
+    def findHands(self, img, draw=True):
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.results = self.hands.process(imgRGB)
+        if self.results.multi_hand_landmarks:
+            for handLms in self.results.multi_hand_landmarks:
+                if draw:
+                    self.mpDraw.draw_landmarks(img, handLms,self.mpHands.HAND_CONNECTIONS)                                               
+        return img
+
+
+
+    def findPosition(self, img, handNo=0, draw=True):
+
+        lmList = []
+        if self.results.multi_hand_landmarks:
+            myHand = self.results.multi_hand_landmarks[handNo]
+            for id, lm in enumerate(myHand.landmark):
+                h, w, c = img.shape
+                cx, cy, cz = int(lm.x * w), int(lm.y * h), int(lm.z * c)
+                print(id, cx, cy, cz)
+        return lmList
+
+
+def main():
+    pTime = 0
+    cTime = 0
+    cap = cv2.VideoCapture(0)
+    detector = handDetector()
+    while True:
+        success, img = cap.read()
+
+        img = detector.findHands(img)
+        lmList = detector.findPosition(img)
+        if len(lmList) != 0:
+            print(lmList)
+
+        cTime = time.time()
+        fps = 1 / (cTime - pTime)
+        pTime = cTime
+
+        cv2.putText(img, str(int(fps)), (8, 60), cv2.FONT_ITALIC, 1,(255, 0, 255), 3)
+        cv2.imshow("Image", img)
+
+
+        if cv2.waitKey(1) == 13:
+            break
+    cv2.destroyWindow()
+
+
+if __name__ == "__main__":
+    main()
