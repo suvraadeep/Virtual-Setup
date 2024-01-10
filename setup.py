bc21b72de3975fa8f6a0fd6a276da8a419281f26
suvraadeep <dassuvradeep9@gmail.com>
2024-01-10 19:01:55 +0530

    added

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     version='0.0.1',
     author='suvradeep',
     author_email='dassuvradeep9@gmail.com',
-    install_requires=["opencv-contrib-python==4.7.0.72","opencv-python==4.8.0.74","opencv-python-headless==4.8.0.74","mediapipe","streamlit","gesture","pyautogui"],
+    install_requires=["opencv-contrib-python==4.7.0.72","opencv-python==4.8.0.74","opencv-python-headless==4.8.0.74","mediapipe","streamlit","gesture","pyautogui","google-generativeai"],
     packages=find_packages()
 )
